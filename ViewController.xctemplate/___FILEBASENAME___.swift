1c9c72c38cc4c28c3cadbac16e74ebd37cc633cd
Alexander Spirichev <a.spirichev@dodopizza.com>
2020-02-09 18:52:56 +0300

    Add template for ViewController with presenter

---
+++ ViewController.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,26 @@
+//___FILEHEADER___
+
+import UIKit
+
+public protocol ___FILEBASENAMEASIDENTIFIER___UIProtocol: AnyObject {
+    
+}
+
+final class ___FILEBASENAMEASIDENTIFIER___ViewController: UIViewController {
+    
+    public var presenter: ___FILEBASENAMEASIDENTIFIER___PresenterProtocol!
+    
+    // MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
+// MARK: - ___FILEBASENAMEASIDENTIFIER___UIProtocol
+
+extension ___FILEBASENAMEASIDENTIFIER___ViewController: ___FILEBASENAMEASIDENTIFIER___UIProtocol {
+    
+}
