31ec45681a145aa7a87c4ef83d16ac7a5b4351d3
Alexander Spirichev <a.spirichev@dodopizza.com>
2020-02-09 17:49:29 +0300

    Add template for Presenter

---
+++ Presenter.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,18 @@
+//___FILEHEADER___
+
+import Foundation
+
+public protocol ___FILEBASENAMEASIDENTIFIER___PresenterProtocol {
+    
+}
+
+final class ___FILEBASENAMEASIDENTIFIER___Presenter: ___FILEBASENAMEASIDENTIFIER___PresenterProtocol {
+    
+    weak var ui: ___VARIABLE_viewProtocol___Protocol?
+    
+    init(ui: ___VARIABLE_viewProtocol___Protocol) {
+        self.ui = ui
+    }
+    
+    // MARK: - Presenter's methods
+}
