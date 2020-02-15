e17ff2bd5728621813d8837f3c84cd717fe518d7
Alexander Spirichev <alspirichev@gmail.com>
2020-02-15 20:50:22 +0300

    Add interactor template

---
+++ Interactor.xctemplate/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,20 @@
+//___FILEHEADER___
+
+import UIKit
+
+public protocol ___VARIABLE_productName:identifier___InteractorProtocol {
+    
+}
+
+final class ___VARIABLE_productName:identifier___Interactor: ___VARIABLE_productName:identifier___InteractorProtocol {
+    
+    weak var presenter: ___VARIABLE_productName:identifier___PresenterProtocol!
+    
+    init() {
+        
+    }
+    
+    // MARK: - Interactor's methods
+    
+    
+}
