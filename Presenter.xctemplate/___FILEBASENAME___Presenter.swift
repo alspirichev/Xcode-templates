71c1d3e92df291fa6a3f8ea2866815ed48de28b3
Alexander Spirichev <alspirichev@gmail.com>
2020-02-29 23:29:23 +0300

    Add ViewController in Router template

--- Presenter.xctemplate/___FILEBASENAME___Presenter.swift
+++ Presenter.xctemplate/___FILEBASENAME___Presenter.swift
@@ -9,11 +9,14 @@ public protocol ___VARIABLE_productName:identifier___PresenterProtocol: AnyObjec
 final class ___VARIABLE_productName:identifier___Presenter: ___VARIABLE_productName:identifier___PresenterProtocol {
     
     weak var ui: ___VARIABLE_productName:identifier___UIProtocol?
+    private let router: ___VARIABLE_productName:identifier___RouterProtocol!
     private let interactor: ___VARIABLE_productName:identifier___InteractorProtocol!
     
     init(ui: ___VARIABLE_productName:identifier___UIProtocol,
+        router: ___VARIABLE_productName:identifier___RouterProtocol,
         interactor: ___VARIABLE_productName:identifier___InteractorProtocol) {
         self.ui = ui
+        self.router = router
         self.interactor = interactor
     }
     
--- Router.xctemplate/___FILEBASENAME___Router.swift
+++ Router.xctemplate/___FILEBASENAME___Router.swift
@@ -8,8 +8,10 @@ public protocol ___VARIABLE_productName:identifier___RouterProtocol {
 
 final class ___VARIABLE_productName:identifier___Router: ___VARIABLE_productName:identifier___RouterProtocol {
     
-    init() {
-        
+    private weak let viewController: ___VARIABLE_productName:identifier___ViewController!
+    
+    init(viewController: ___VARIABLE_productName:identifier___ViewController) {
+        self.viewController = viewController
     }
     
     // MARK: - Router's methods
