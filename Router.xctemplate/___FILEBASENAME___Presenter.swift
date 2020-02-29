d2e786f17232d5dfbc7ca89f597ed8d0f95d946a
Alexander Spirichev <alspirichev@gmail.com>
2020-02-29 21:26:47 +0300

    Add template for Router

---
+++ Router.xctemplate/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,18 @@
+//___FILEHEADER___
+
+import Foundation
+
+public protocol ___VARIABLE_productName:identifier___RouterProtocol {
+    
+}
+
+final class ___VARIABLE_productName:identifier___Router: ___VARIABLE_productName:identifier___RouterProtocol {
+    
+    init() {
+        
+    }
+    
+    // MARK: - Router's methods
+    
+    
+}
