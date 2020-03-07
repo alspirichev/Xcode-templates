23c7c6f99c1ef8583e7e35fbb071721b53c13e51
Alexander Spirichev <alspirichev@gmail.com>
2020-03-07 18:35:00 +0300

    Replace weak let on var

--- VIPER/Router.xctemplate/___FILEBASENAME___Router.swift
+++ VIPER/Router.xctemplate/___FILEBASENAME___Router.swift
@@ -8,7 +8,7 @@ public protocol ___VARIABLE_productName:identifier___RouterProtocol {
 
 final class ___VARIABLE_productName:identifier___Router: ___VARIABLE_productName:identifier___RouterProtocol {
     
-    private weak let viewController: ___VARIABLE_productName:identifier___ViewController!
+    private weak var viewController: ___VARIABLE_productName:identifier___ViewController!
     
     init(viewController: ___VARIABLE_productName:identifier___ViewController) {
         self.viewController = viewController
