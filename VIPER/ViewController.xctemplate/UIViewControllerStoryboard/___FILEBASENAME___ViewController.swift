0e8950a581fdfa8c56bbe460d314f00ad6801224
Alexander Spirichev <alspirichev@gmail.com>
2020-03-08 13:36:22 +0300

    Add storyboard for ViewController template

---
+++ VIPER/ViewController.xctemplate/UIViewControllerStoryboard/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,28 @@
+//___FILEHEADER___
+
+import UIKit
+import DodoUIKit
+
+public protocol ___VARIABLE_productName:identifier___UIProtocol: AnyObject {
+    
+}
+
+final public class ___VARIABLE_productName:identifier___ViewController: ___VARIABLE_cocoaTouchSubclass___, ViewSpecificController, Storyboardable {
+    public typealias RootView = ___VARIABLE_productName:identifier___View
+    
+    public var presenter: ___VARIABLE_productName:identifier___PresenterProtocol!
+    
+    // MARK: - Life cycle
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
+// MARK: - ___VARIABLE_productName:identifier___UIProtocol
+
+extension ___VARIABLE_productName:identifier___ViewController: ___VARIABLE_productName:identifier___UIProtocol {
+    
+}
---
+++ VIPER/ViewController.xctemplate/UIViewControllerViewStoryboard/___FILEBASENAME___View.swift
@@ -0,0 +1,8 @@
+//___FILEHEADER___
+
+import UIKit
+
+final public class ___VARIABLE_productName:identifier___View: UIView {
+    
+    
+}
