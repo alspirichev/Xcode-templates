a8874689735ec3a0712cc99b9c6ba38d747091f0
Alexander Spirichev <alspirichev@gmail.com>
2020-03-09 22:34:13 +0300

    Add comment to View template

--- VIPER/ViewController.xctemplate/UIViewControllerViewStoryboard/___FILEBASENAME___View.swift
+++ VIPER/ViewController.xctemplate/UIViewControllerViewStoryboard/___FILEBASENAME___View.swift
@@ -4,6 +4,8 @@ import UIKit
 
 final public class ___VARIABLE_viewControllerName:identifier___View: UIView {
     
+    // keep your IBOutlet's private
+    
     public override func awakeFromNib() {
         super.awakeFromNib()
         
