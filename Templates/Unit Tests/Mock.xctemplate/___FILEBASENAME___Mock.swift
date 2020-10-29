07af0ba61a8ff763e34b8eb2109f6870d3b133a4
Alexander Spirichev <alspirichev@gmail.com>
2020-10-29 22:57:46 +0300

    Add example for spry

--- Templates/Unit Tests/Mock.xctemplate/___FILEBASENAME___Mock.swift
+++ Templates/Unit Tests/Mock.xctemplate/___FILEBASENAME___Mock.swift
@@ -1,7 +1,7 @@
 @testable import <#Framework#>
 import Spry
 
-class ___VARIABLE_productName:identifier___Mock: Spryable {
+final class ___VARIABLE_productName:identifier___Mock: Spryable {
     enum ClassFunction: String, StringRepresentable {
         case empty
     }
@@ -9,4 +9,42 @@ class ___VARIABLE_productName:identifier___Mock: Spryable {
     enum Function: String, StringRepresentable {
         case <#your case#>
     }
+    
+    // Example
+    
+    // enum ClassFunction: String, StringRepresentable { // <-- **REQUIRED**
+    //    case giveMeAString = "giveMeAString(arg1:arg2:)"
+    // }
+    //
+    // enum Function: String, StringRepresentable { // <-- **REQUIRED**
+    //    case readonlyProperty = "readonlyProperty"
+    //    case readwriteProperty = "readwriteProperty"
+    //    case doThings = "doThings()"
+    //    case giveMeAString = "giveMeAString(arg1:arg2:)"
+    // }
+    //
+    // var readonlyProperty: String {
+    //     return stubbedValue()
+    // }
+    //
+    // var readwriteProperty: String {
+    //     set {
+    //         recordCall(arguments: newValue)
+    //     }
+    //     get {
+    //         return stubbedValue()
+    //     }
+    // }
+    //
+    // func doThings() {
+    //     return spryify() // <-- **REQUIRED**
+    // }
+    //
+    // func giveMeAString(arg1: Bool, arg2: String) -> String {
+    //     return spryify(arguments: arg1, arg2) // <-- **REQUIRED**
+    // }
+    //
+    // class func giveMeAString(arg1: Bool, arg2: String) -> String {
+    //     return spryify(arguments: arg1, arg2) // <-- **REQUIRED**
+    // }
 }
