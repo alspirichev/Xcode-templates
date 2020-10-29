c73443200e427ece46bffdaadd1e5401c8f95192
Alexander Spirichev <alspirichev@gmail.com>
2020-10-29 23:30:57 +0300

    Add script for install

---
+++ install_snippets_and_template.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/Library/Developer/Xcode/UserData/CodeSnippets
+cp ./Snippets/* ~/Library/Developer/Xcode/UserData/CodeSnippets
+
+mkdir -p ~/Library/Developer/Xcode/Templates/Files\ Templates/
+cp -r ./Templates/ ~/Library/Developer/Xcode/Templates/Files\ Templates/
+
+echo "Done 🎉"
