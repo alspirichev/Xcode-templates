924c1293ed4b2d46e8b92de75a44c319f475563b
Alexander Spirichev <alspirichev@gmail.com>
2021-02-22 16:32:36 +0300

    Add template for analytic event

---
+++ Templates/Helpers/Event.xctemplate/___FILEBASENAME___Event.swift
@@ -0,0 +1,27 @@
+import DAnalytics
+
+public class ___VARIABLE_productName:identifier___Event: AnalyticsEvent {
+    public typealias ContextType = ___VARIABLE_productName:identifier___Event.Context
+
+    public let supportedProviders: [AnalyticsProviderType] = [.kusto]
+
+    public let sender: AnalyticsSenderProtocol
+
+    init(sender: AnalyticsSenderProtocol) {
+        self.sender = sender
+    }
+
+    public func log(with context: Context) {
+        sender.logEvent(<#event_name#>,
+                        parameters: context.parameters,
+                        supportedProviders: supportedProviders)
+    }
+}
+
+extension ___VARIABLE_productName:identifier___Event {
+    public struct Context: AnalyticsContext {
+        public var parameters: AnalyticsParameters {
+            [:]
+        }
+    }
+}
